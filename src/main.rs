f42f99efc85d96d8afaa7f7ecf5721443d160930
Ivo Georgiev <ivo@strem.io>
2019-02-22 18:45:22 +0200

    add ID

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use stremio_state_ng::types::*;
 
 const TYPE_STR: &str = "channel";
 const BLITZ_BASE: &str = "https://www.blitz.bg";
+const INVALID_ID: &str = "blitz-invalid-id";
+const POSTER_SHAPE: &str = "landscape";
+
 const MANIFEST_RAW: &str = include_str!("../manifest.json");
 
 #[get("/manifest.json")]
@@ -35,10 +38,11 @@ fn scrape_blitz() -> Result<Vec<MetaPreview>, Box<dyn Error>> {
     Ok(Document::from_read(resp)?
         .find(Name("article"))
         .map(|article| MetaPreview{
-            id: "".to_owned(),
+            id: get_id_from_article(&article).unwrap_or(INVALID_ID.to_owned()),
             poster: get_poster_from_article(&article),
             type_name: TYPE_STR.to_owned(),
             name: article.text().trim().to_string(),
+            poster_shape: Some(POSTER_SHAPE.to_owned()),
         })
         .collect()
     )
@@ -51,8 +55,12 @@ fn get_poster_from_article(article: &select::node::Node) -> Option<String> {
         .map(|s| s.to_owned())
 }
 
-//fn get_id_from_article(article: &select::node::Node) -> String {
-//}
+fn get_id_from_article(article: &select::node::Node) -> Option<String> {
+    article.find(Name("a"))
+        .next()?
+        .attr("href")
+        .map(|s| s.split("/").skip(3).collect::<Vec<&str>>().join("/"))
+}
 
 fn main() {
         rocket::ignite().mount("/", routes![catalog]).launch();
